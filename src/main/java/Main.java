15d1790e47e77ab4967c0b241d393f49d9dd8314
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-25 19:27:09 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
 public class Main {
-  public static void main(String[] args) {
-    APLine line1 = new APLine(5, 4, -17);
-    double slope1 = line1.getSlope();
-    boolean onLine1 = line1.isOnLine(5, -2);
-    System.out.println(slope1 + " " + onLine1);
-
-    APLine line2 = new APLine(-25, 40, 30);
-    double slope2 = line2.getSlope();
-    boolean onLine2 = line2.isOnLine(5, -2);
-    System.out.println(slope2 + " " + onLine2);
+  public static void main(String[] args)
+  {
+    System.out.println("******* A-1 *******");
+    WordList w = new WordList();
+    w.add("cat");
+    w.add("mouse");
+    w.add("frog");
+    w.add("dog");
+    w.add("dog");
+    System.out.println(w + " " +
+           w.numWordsOfLength(4) + " " +
+           w.numWordsOfLength(3) + " " +
+           w.numWordsOfLength(2));
+    System.out.println("******* A-2 *******");
+    w.removeWordsOfLength(4);
+    System.out.println(w);
+    w.removeWordsOfLength(3);
+    System.out.println(w);
+    w.removeWordsOfLength(2);
+    System.out.println(w);
   }
+
 }
